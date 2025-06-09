9650ad1bc503357f9a7717fa5407ef02f5abf2d2
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:59:14 -0300

    Implementado ProdutoRepository para gerenciar produtos.

---
+++ ProdutoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace P2_Prova_de_POO
+{
+    public class ProdutoRepository : IRepository<Produto>
+    {
+        private readonly List<Produto> _produtos = new List<Produto>();
+
+        public void Adicionar(Produto produto) => _produtos.Add(produto);
+
+        public Produto ObterPorId(int id) => _produtos.FirstOrDefault(p => p.Id == id);
+
+        public List<Produto> ObterTodos() => new List<Produto>(_produtos);
+    }
+}
