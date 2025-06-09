fcf7ce8d1824169522b538f2ea6d4ece7f6bc580
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:17:10 -0300

    Adicionada estratégia de desconto por quantidade, aplicando 5% para itens com 5 ou mais unidades.

---
+++ DescontoPorQuantidade.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace P2_Prova_de_POO
+{
+    public class DescontoPorQuantidade : IDescontoStrategy
+    {
+        public decimal AplicarDesconto(Pedido pedido)
+        {
+            return pedido.Itens
+                .Where(i => i.Quantidade >= 5)
+                .Sum(i => i.Subtotal() * 0.05m);
+        }
+    }
+}
