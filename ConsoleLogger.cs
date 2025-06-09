6da6df3163cef8ad880a303c1d1eb904b12d90bf
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 17:01:19 -0300

    Delete ConsoleLogger.cs

--- ConsoleLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace P2_Prova_de_POO
-{
-    public class ConsoleLogger : ILogger
-    {
-        public void Log(string mensagem) => Console.WriteLine("[LOG] " + mensagem);
-    }
-}
