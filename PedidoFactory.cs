1d4fa1e375a61dd485eba115a4c0367d0d2fbc54
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:52:20 -0300

    Criada fábrica de pedidos para facilitar a criação de instâncias de Pedido com estratégia de desconto e log.

---
+++ PedidoFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P2_Prova_de_POO
+{
+    public class PedidoFactory
+    {
+        private readonly ILogger logger;
+
+        public PedidoFactory() { }
+
+        public PedidoFactory(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public Pedido CriarPedido(Cliente cliente, List<ItemPedido> itens, IDescontoStrategy descontoStrategy)
+        {
+            return new Pedido
+            {
+                Cliente = cliente,
+                Itens = itens,
+                DescontoStrategy = descontoStrategy,
+                Data = DateTime.Now
+            };
+        }
+    }
+}
