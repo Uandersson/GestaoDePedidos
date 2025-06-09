48d38a1d2f94adc5d77e81cdbd597898bf684baf
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:49:17 -0300

    Classe Pedido implementada com lista de itens, cliente, data, e cálculo total com desconto aplicado.

---
+++ Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P2_Prova_de_POO
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<ItemPedido> Itens { get; set; } = new List<ItemPedido>();
+        public DateTime Data { get; set; } = DateTime.Now;
+        public IDescontoStrategy DescontoStrategy { get; set; }
+
+        public decimal CalcularTotal()
+        {
+            var total = Itens.Sum(item => item.Subtotal());
+            var desconto = DescontoStrategy?.AplicarDesconto(this) ?? 0;
+            return total - desconto;
+        }
+    }
+}
