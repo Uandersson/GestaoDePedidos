6eb4a9b47bfdb4269137e0c34462d6862b1db1ed
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:14:58 -0300

    Add files via upload
    Criado ConsoleLogger simples para exibir mensagens de log no console.

---
+++ ConsoleLogger.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace P2_Prova_de_POO
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string mensagem) => Console.WriteLine("[LOG] " + mensagem);
+    }
+}
