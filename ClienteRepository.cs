3940c20363ff2f7d1664a0f5ddb4f2696098a71e
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:10:33 -0300

    Add files via upload
    Implementado ClienteRepository com métodos para adicionar clientes, buscar por ID e listar todos.

---
+++ ClienteRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace P2_Prova_de_POO
+{
+    public class ClienteRepository : IRepository<Cliente>
+    {
+        private readonly List<Cliente> _clientes = new List<Cliente>();
+
+        public void Adicionar(Cliente cliente) => _clientes.Add(cliente);
+
+        public Cliente ObterPorId(int id) => _clientes.FirstOrDefault(c => c.Id == id);
+
+        public List<Cliente> ObterTodos() => new List<Cliente>(_clientes);
+    }
+}
