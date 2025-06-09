e360519ea735d8be74ca91e0180287b811e5bd84
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:56:57 -0300

    Classe Produto adicionada com validação simples no preço e propriedades básicas.

---
+++ Produto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P2_Prova_de_POO
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+
+        public Produto(int id, string nome, decimal preco, string categoria)
+        {
+            if (preco <= 0) throw new ArgumentException("Preço inválido!");
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+
+        public Produto(string nome, decimal preco, string categoria)
+        {
+            Nome = nome;
+            Preco = preco;
+            Categoria = categoria;
+        }
+    }
+}
