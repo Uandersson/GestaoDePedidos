8a4a9bc2f4473deac5b33a1fdb946c9f1d529477
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:44:33 -0300

    Classe ItemPedido adicionada, representando um produto e sua quantidade no pedido, com cálculo de subtotal.

---
+++ ItemPedido.cs
@@ -0,0 +1,16 @@
+﻿namespace P2_Prova_de_POO
+{
+    public class ItemPedido
+    {
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            Produto = produto;
+            Quantidade = quantidade;
+        }
+
+        public decimal Subtotal() => Produto.Preco * Quantidade;
+    }
+}
