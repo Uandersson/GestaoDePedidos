70c80182ede797f354bb7186aa948acf83c0a9a4
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:41:59 -0300

    Interface genérica IRepository criada para manipulação básica de repositórios.

---
+++ IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace P2_Prova_de_POO
+{
+    public interface IRepository<T>
+    {
+        void Adicionar(T item);
+        T ObterPorId(int id);
+        List<T> ObterTodos();
+    }
+}
