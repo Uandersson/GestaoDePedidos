92b7033c40f3583d032778cc5d733f24aa5f63e2
Uandersson <Santosribeirou123@gmail.com>
2025-06-09 16:54:54 -0300

    Implementado PedidoRepository para armazenar e buscar pedidos.

---
+++ PedidoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace P2_Prova_de_POO
+{
+    public class PedidoRepository : IRepository<Pedido>
+    {
+        private readonly List<Pedido> _pedidos = new List<Pedido>();
+
+        public void Adicionar(Pedido pedido) => _pedidos.Add(pedido);
+
+        public Pedido ObterPorId(int id) => _pedidos.FirstOrDefault(p => p.Id == id);
+
+        public List<Pedido> ObterTodos() => new List<Pedido>(_pedidos);
+    }
+}
